946447c44a9673c6ccdc7e72ea0695c4ba877f77
Siddhesh Surve <sysurve24@gmail.com>
2023-04-09 05:41:28 +0000

    map func

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
-
 function ListGroup() {
+  var items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  if(items.length == 0) 
+    return(
+        <>
+            <h1>List</h1>
+            <p>No Item found!</p>
+        </>
+    );
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
